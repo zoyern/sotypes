78a188240e00cc1e7c1b67af2e60e163954ce994
Zoyerny <zoyern@gmail.com>
2024-05-04 17:04:36 +0200

    add somalloc and sofree to head.h

--- src/sotypes.h
+++ src/sotypes.h
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 17:59:30 by marvin            #+#    #+#             */
-/*   Updated: 2024/04/30 18:02:51 by marvin           ###   ########.fr       */
+/*   Updated: 2024/05/04 17:02:58 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include <sotypes/types.h>
 
 t_solib	*sonew_types(int argc, char **argv, char **envp);
+void			*somalloc(t_solib *solib, size_t size);
+int				sofree(t_solib *solib, void *ptr);
 
 #endif
