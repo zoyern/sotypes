51a6d51752b78436ad6361b12b63cebc3548462f
zoyern <zoyern@gmail.com>
2024-10-22 18:05:48 +0200

    adding sotask to main .h

--- build/includes/sotypes.h
+++ build/includes/sotypes.h
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 17:59:30 by marvin            #+#    #+#             */
-/*   Updated: 2024/10/22 17:58:12 by marvin           ###   ########.fr       */
+/*   Updated: 2024/10/22 18:04:15 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ t_sotask	*sonew_task(t_solib *solib, char *time, int (*callback)(),
 				void *data);
 t_sotasks	*sotask_list(t_solib *solib);
 void		sotask_add(t_sotasks *list, unsigned long id, t_sotask *task);
+int			sotask(long time, t_sotasks *tasks, void *data);
 
 #endif
